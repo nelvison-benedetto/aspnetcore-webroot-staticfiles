63396510e3bb573f5fa4f764cbb1d42e123c2267
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2025-12-14 11:30:23 +0100

    update

--- Program.cs
+++ Program.cs
@@ -6,9 +6,13 @@ namespace _3_StaticFilesExample
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
-
+            
+            app.UseStaticFiles();
+            
             app.MapGet("/", () => "Hello World!");
 
+            //http://localhost:5000/
+            //http://localhost:5000/imgCertificationPythonComputerVision.jpg
             app.Run();
         }
     }
