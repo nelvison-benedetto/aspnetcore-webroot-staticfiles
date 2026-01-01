594e757f495b609d76d0f18b6cd021e494086d6f
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-01 16:05:09 +0100

    done

--- Program.cs
+++ Program.cs
@@ -1,19 +1,27 @@
+using Microsoft.Extensions.FileProviders;
+
 namespace _3_StaticFilesExample
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(new WebApplicationOptions() { 
+                WebRootPath = "wwwroot"  //impostiamo la cartella wwwroot come cartella per i file statici
+            });
             var app = builder.Build();
             
-            app.UseStaticFiles();
-            
+            app.UseStaticFiles(); //w this tutti i files statici nella cartella wwwroot sono accessibili tramite diretto url req
+            app.UseStaticFiles(new StaticFileOptions() {  //static file middlewarare
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(builder.Environment.ContentRootPath, "mywwwroot")
+                ),
+            });
             app.MapGet("/", () => "Hello World!");
 
-            //http://localhost:5000/
-            //http://localhost:5000/imgCertificationPythonComputerVision.jpg
-            //http://localhost:5000/myimg.jpg
+            //http://localhost:xxx/img1.jpg
+            //http://localhost:xxx/img2.jpg
+            //http://localhost:xxx/img3.jpg
             app.Run();
         }
     }
